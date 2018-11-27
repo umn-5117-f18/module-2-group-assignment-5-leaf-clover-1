b7a25b31a4b300222c42ab82c40bb9aae6ff7b51
Bridger Herman <herma582@umn.edu>
2018-11-27 17:37:33 -0600

    wip

--- app.ly/src/components/ResumeSection.vue
+++ app.ly/src/components/ResumeSection.vue
@@ -1,12 +1,36 @@
 <template>
-  <h3>I am a Section {{ msg }}</h3>
+  <div class="resume-section" v-html="madeHtml">
+  </div>
 </template>
 
 <script>
 export default {
   name: 'ResumeSection',
+
   props: {
-    msg: String
+    level: Number,
+    text: String,
+  },
+
+  computed: {
+    madeHtml: function() {
+      switch (this.level) {
+        case 1:
+          return this.makeElement('h2');
+        case 2:
+          return this.makeElement('h3');
+        case 3:
+          return this.makeElement('h4');
+        default:
+          return this.makeElement('p');
+      }
+    },
+  },
+
+  methods: {
+    makeElement: function(type) {
+      return '<' + type + '>' + this.text + '</' + type + '>';
+    },
   },
 }
 </script>
--- app.ly/src/views/MasterResume.vue
+++ app.ly/src/views/MasterResume.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="master-resume">
-    <button a class="button" v-on:click="logout">Sign Out</button>
-    <h2>I am the master resume</h2>
-    <article v-for="item in ['Education', 'Work Experience', 'Skills']" :key="item" >
+    <h2>Your Master Resume</h2>
+    <article v-for="item in resumeSections">
       <ResumeSection v-bind:msg="item"/>
     </article>
     <Footer/>
@@ -10,12 +9,13 @@
 </template>
 
 <script>
-import ResumeSection from '@/components/ResumeSection.vue'
 import firebase from 'firebase'
-
+import ResumeSection from '@/components/ResumeSection.vue'
+import { testResume } from '@/testResume'
 
 export default {
   name: 'MasterResume',
+
   components: {
     ResumeSection
   },
