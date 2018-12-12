8495c52a34d4336a0c8fd9654566496fc5f896fe
Bridger Herman <herma582@umn.edu>
2018-12-11 20:46:17 -0600

    Make file upload callback an arrow function

--- app.ly/src/components/Camera.vue
+++ app.ly/src/components/Camera.vue
@@ -18,7 +18,7 @@
     methods: {
       onChange(files) {
         var uploadTask = storageRef.child('images/' + firebase.auth().currentUser.uid + '-' + this.$route.params.id).put(files[0]);
-        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
           console.log('File available at', downloadURL);
           this.imgUrl = downloadURL;
         });
